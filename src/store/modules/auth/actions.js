ba78263db000a0cec2d367a46d40071ba12ed812
NaluFigueira <anachfigueira@gmail.com>
2019-12-14 14:21:42 -0300

    Adicionado cadastro, header e esquema de notificações

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -17,3 +17,20 @@ export function signInFailure() {
     type: '@auth/SIGN_IN_FAILURE',
   };
 }
+
+export function signUpRequest(name, email, password) {
+  return {
+    type: '@auth/SIGN_UP_REQUEST',
+    payload: {
+      name,
+      email,
+      password,
+    },
+  };
+}
+
+export function signUpFailure() {
+  return {
+    type: '@auth/SIGN_UP_FAILURE',
+  };
+}
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess, signInFailure } from './actions';
+import { signInSuccess, signInFailure, signUpFailure } from './actions';
 
 export function* signIn({ payload }) {
   try {
@@ -22,6 +22,8 @@ export function* signIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
@@ -31,4 +33,36 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha no cadastro, verifique seus dados!');
+    yield put(signUpFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
